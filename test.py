6130027b6ffadeec1905ed914dc564e6ab285f7b
@XiaoRen123 <1602478902@qq.com>
2019-04-07 15:02:11 +0800

    add python base

---
+++ test.py
@@ -0,0 +1,24 @@
+"""
+#_*_coding:utf-8_*_
+Name:test.py   
+Date:4/7/19
+Author:westos-wsp
+Connect:wsp432@sina.com
+Desc:
+"""
+
+print('eeee')
+print('hello'+'wwww')
+print('DDDD','DDD')
+print('hello','dddd','!',123,sep='***')
+print('hello','wwoe','!',123,end='')
+
+user =input('username:')
+print(user)
+
+help(input)
+
+a=input('number:')
+print(int(a)+5)
+print(a+str(3))
+
---
+++ testlogin.py
@@ -0,0 +1,16 @@
+"""
+#_*_coding:utf-8_*_
+Name:testlogin.py.py   
+Date:4/7/19
+Author:westos-wsp
+Connect:wsp432@sina.com
+Desc:
+"""
+# username = input("username:")
+# print('Welcome', username)
+# print('Welcome' + username)
+# PEP8
+a = 10 + 5
+a = a + 10
+print(a)
+
